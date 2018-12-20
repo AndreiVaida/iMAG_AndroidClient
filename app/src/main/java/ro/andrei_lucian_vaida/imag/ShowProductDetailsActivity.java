f05a954cbf76fcd13dea7ab7c047f2785cc45297
AndreiVaida <andrei_vd2006@yahoo.com>
2018-12-20 10:55:30 +0200

    Show all products working. Show product details working.

---
+++ app/src/main/java/ro/andrei_lucian_vaida/imag/ShowProductDetailsActivity.java
@@ -0,0 +1,73 @@
+package ro.andrei_lucian_vaida.imag;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class ShowProductDetailsActivity extends AppCompatActivity {
+    private Integer productId;
+    private TextView productNameView;
+    private TextView productPriceView;
+    private TextView productDetailsView;
+    private final String productsUrl = "/product";
+    private RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_product_details);
+        productNameView = findViewById(R.id.productNameView);
+        productPriceView = findViewById(R.id.productPriceView);
+        productDetailsView = findViewById(R.id.productDetailsView);
+
+        queue = Volley.newRequestQueue(this);
+        Intent intent = getIntent();
+        productId = intent.getIntExtra("productId", -1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadProductDetails(productId);
+    }
+
+    private void loadProductDetails(final Integer productId) {
+        final JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                MainActivity.serverUrl + productsUrl + "/" + productId,
+                null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            productNameView.setText(response.getString("name"));
+                            productPriceView.setText(response.getString("price") + " lei");
+                            productDetailsView.setText(response.getString("details"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        productNameView.setText("Produs indisponibil");
+                        productDetailsView.setText(error.getMessage());
+                    }
+                });
+        queue.add(jsonObjectRequest);
+    }
+}
--- app/src/main/java/ro/andrei_lucian_vaida/imag/ShowProductsActivity.java
+++ app/src/main/java/ro/andrei_lucian_vaida/imag/ShowProductsActivity.java
@@ -1,7 +1,14 @@
 package ro.andrei_lucian_vaida.imag;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -11,39 +18,62 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.view.Gravity.CENTER_VERTICAL;
 
 public class ShowProductsActivity extends AppCompatActivity {
     private final String productsUrl = "/product";
     private RequestQueue queue;
     private TextView title;
+    private LinearLayout productsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_products);
+        // initialize components from XML
         title = findViewById(R.id.title);
-        System.out.println();
-        System.out.println("onCreate");
-        System.out.println();
+        productsLayout = findViewById(R.id.productslayout);
+        queue = Volley.newRequestQueue(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        System.out.println();
-        System.out.println("onStart");
-        System.out.println();
-        queue = Volley.newRequestQueue(this);
         loadProducts();
     }
 
+    private void goToProductDetailsActivity(Integer productId) {
+        Intent intent = new Intent(this, ShowProductDetailsActivity.class);
+        intent.putExtra("productId", productId);
+        startActivity(intent);
+    }
+
     private void loadProducts() {
-        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(MainActivity.serverUrl + productsUrl,
+        final JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(MainActivity.serverUrl + productsUrl,
                 new Response.Listener<JSONArray>() {
 
                     @Override
                     public void onResponse(JSONArray response) {
-                        title.setText("Response: " + response.toString());
+                        productsLayout.removeAllViews();
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                // get JSON product
+                                final JSONObject jsonProduct = response.getJSONObject(i);
+
+                                // create a new layout for the product
+                                final LinearLayout productLayout = createNewProductLayout(jsonProduct);
+
+                                // add the product view to the page
+                                productsLayout.addView(productLayout);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
                     }
                 }, new Response.ErrorListener() {
 
@@ -53,7 +83,52 @@ public class ShowProductsActivity extends AppCompatActivity {
                     }
                 });
 
-        // Access the RequestQueue through your singleton class.
-        queue.add(jsonObjectRequest);
+        queue.add(jsonArrayRequest);
+    }
+
+    private LinearLayout createNewProductLayout(final JSONObject jsonProduct) throws JSONException {
+        // image
+        final byte[] byteArray = jsonProduct.getString("image").getBytes();
+        final Bitmap bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+        final ImageView imageView = new ImageView(ShowProductsActivity.this);
+        imageView.setImageBitmap(bmp);
+        // name
+        final TextView productNameView = new TextView(ShowProductsActivity.this);
+        productNameView.setText("Denumire: " + jsonProduct.getString("name"));
+        // price
+        final TextView productPriceView = new TextView(ShowProductsActivity.this);
+        productPriceView.setText("Preț: " + jsonProduct.getString("price"));
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(200, 0, 0, 0);
+        productPriceView.setLayoutParams(params);
+
+        // create a new layout for the product
+        final LinearLayout productLayout = new LinearLayout(ShowProductsActivity.this);
+        productLayout.setBackgroundColor(Color.parseColor("#91bbd1"));
+        productLayout.setPadding(5, 5, 5, 5);
+        productLayout.setGravity(CENTER_VERTICAL);
+        final LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 5, 0, 5);
+        layoutParams.height = 100;
+        productLayout.setLayoutParams(layoutParams);
+        productLayout.setOrientation(LinearLayout.HORIZONTAL);
+        productLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    final Integer productId = jsonProduct.getInt("id");
+                    goToProductDetailsActivity(productId);
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        // populate the product layout
+        productLayout.addView(imageView);
+        productLayout.addView(productNameView);
+        productLayout.addView(productPriceView);
+
+        return productLayout;
     }
 }
