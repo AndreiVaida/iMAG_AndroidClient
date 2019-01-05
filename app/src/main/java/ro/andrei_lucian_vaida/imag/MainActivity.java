65ff2b5d058c0f11bfcce5f89111dfb698945f9a
AndreiVaida <andrei_vd2006@yahoo.com>
2019-01-05 21:54:45 +0200

    Added animations to MainActivity buttons.

--- app/src/main/java/ro/andrei_lucian_vaida/imag/MainActivity.java
+++ app/src/main/java/ro/andrei_lucian_vaida/imag/MainActivity.java
@@ -3,36 +3,61 @@ package ro.andrei_lucian_vaida.imag;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     public static final String serverUrl = "http://10.0.2.2:8080";
+    private Button registerButton;
+    private Button wishlistButton;
     private Button loginButton;
+    private Button showProductsButton;
     private Integer userId;
     private String token;
+    private Animation bounceAnimation;
+    private Animation fadeInAnimation;
+    private Animation leftToRightAnimation;
+    private Animation rightToLeftAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        registerButton = findViewById(R.id.registerButton);
+        wishlistButton = findViewById(R.id.wishlistButton);
         loginButton = findViewById(R.id.loginButton);
+        showProductsButton = findViewById(R.id.showProductsButton);
+        bounceAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce);
+        fadeInAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadein);
+        leftToRightAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.lefttoright);
+        rightToLeftAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.righttoleft);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         updateLoginButton();
+        playAnimations();
+    }
+
+    private void playAnimations() {
+        registerButton.startAnimation(leftToRightAnimation);
+        loginButton.startAnimation(rightToLeftAnimation);
+        wishlistButton.startAnimation(fadeInAnimation);
+        showProductsButton.startAnimation(bounceAnimation);
     }
 
     private void updateLoginButton() {
         getSharedPreferences();
         if (isLoggedIn()) {
             loginButton.setText("Logout");
-        }
-        else {
+        } else {
             loginButton.setText("Login");
         }
     }
@@ -50,8 +75,7 @@ public class MainActivity extends AppCompatActivity {
         if (isLoggedIn()) {
             logout();
             loginButton.setText("Login");
-        }
-        else {
+        } else {
             goToLoginActivity();
         }
     }
